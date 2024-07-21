be705babd6c4d75f4afb80537bfbc01b4a414236
7-7sada <jedsada.chot@gmail.com>
2024-07-21 13:02:30 +0700

    product component

--- src/views/user/HomeView.vue
+++ src/views/user/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import UserLayout from '@/layouts/UserLayout.vue'
+import Product from '@/components/Product.vue';
 import { useProductStore } from '@/stores/user/product';
 
 const productStore = useProductStore();
@@ -18,20 +19,6 @@ const productStore = useProductStore();
             </div>
         </div>
 
-        <!-- Product shelf -->
-         <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-4">
-             <div v-for="product in productStore.list" class="card bg-base-100 w-full shadow-xl">
-                 <figure>
-                     <img class="w-full" :src="product.imageUrl" alt="Shoes" />
-                 </figure>
-                 <div class="card-body">
-                     <h2 class="card-title">{{product.name}}</h2>
-                     <p>{{ product.about}}</p>
-                     <div class="card-actions justify-end">
-                         <button class="btn btn-primary">Buy Now</button>
-                     </div>
-                 </div>
-             </div>
-         </section>
+         <Product :products="productStore.list" />
     </UserLayout>
 </template>
