9cd6b038d9d0992410acb3211c22f14cfbfaa6af
7-7sada <jedsada.chot@gmail.com>
2024-07-24 15:16:55 +0700

    logout - clear data

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -23,6 +23,9 @@ const login = () => {
 const logout = () => {
   isLoggedIn.value = false;
   localStorage.removeItem('isLoggedIn')
+  localStorage.removeItem('cart-data')
+  localStorage.removeItem('order-data')
+  window.location.reload() // refresh เว็บ
 }
 
 const handleSearch = (event) => {
