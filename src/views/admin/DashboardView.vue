9c9760cb4f26702cf71bffd044e12e0a43b759cf
JedsadaChot <jedsada.chot@gmail.com>
2024-07-31 21:33:18 +0700

    setup Apexchart

--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -1,10 +1,27 @@
 <script setup>
 import AdminLayout from "@/layouts/AdminLayout.vue";
+
+const options = {
+  chart: {
+    id: 'vuechart-example'
+  },
+  xaxis: {
+    categories: [1991,1992,1993,1994,1005,1006,1997,1998]
+  }
+}
+
+const series = [{
+  name: 'series-1',
+  data: [30,40,45,50,55,60,70,91]
+}]
 </script>
 
 <template>
   <AdminLayout>
     Dashboard View
+    <div>
+      <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
+    </div>
   </AdminLayout>
 </template>
 
